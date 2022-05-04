e7fddc91de46db0dfa0d87f36032558ddb5a0e10
ibnshayed <emranibnshayed@gmail.com>
2022-05-04 20:22:23 +0600

    nav active select fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,8 +21,9 @@
   }
 
   // Smooth scroll for the navigation menu and links with .scrollto classes
-  $(document).on('click', '.nav-menu a, .scrollto', function(e) {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+	$(document).on('click', '.nav-menu a, .scrollto', function (e) {
+		var $thisLi = $(this).closest('li')
+		if (!$thisLi.hasClass('active') && location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       e.preventDefault();
       var target = $(this.hash);
       if (target.length) {
@@ -34,8 +35,10 @@
         }, 1500, 'easeInOutExpo');
 
         if ($(this).parents('.nav-menu, .mobile-nav').length) {
-          $('.nav-menu .active, .mobile-nav .active').removeClass('active');
-          $(this).closest('li').addClass('active');
+					$('.nav-menu .active, .mobile-nav .active').removeClass('active');
+					setTimeout(function () {
+						$(this).closest('li').addClass('active');
+					}, 200)
         }
 
         if ($('body').hasClass('mobile-nav-active')) {
